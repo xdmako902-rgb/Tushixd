17071a4aca9448d3130b8a5f7fe64d5b7f3baaa7
xdmako902-rgb <xdmako902@gmail.com>
2026-07-14 08:35:16 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,33 @@
+module.exports = {
+    PREFIX: ".",
+    OWNER_NUMBER: "94766398472",
+    BOT_NAME: "BLACK-QUEEN-MD",
+    WORK_TYPE: "public",
+
+    AUTO_TYPING: false,
+    AUTO_RECORDING: true,
+    ALWAYS_ONLINE: false,
+    ALWAYS_OFFLINE: true,
+    AUTO_READ: false,
+    READ_CMD_ONLY: true,
+    AUTO_BIO: true,
+
+    AUTO_REACT: false,
+    REACT_EMOJIS: ['❤️','💕','😻','🧡','💛','💚','💙','💜','🎉','👋'],
+
+    AUTO_READ_STATUS: true,
+    AUTO_VIEW_STATUS: true,
+    AUTO_LIKE_STATUS: true,
+    AUTO_LIKE_EMOJI: ['❤️', '🔥', '👍', '😂', '😮'],
+
+    ANTI_BOT: false,
+    ANTI_BAD: false,
+    ANTI_LINK: false,
+    ANTI_DELETE: "true",
+    DELETEMSGSENDTO: "owner",
+        
+    NEWSLETTER_JID: "120363408157380722@g.usr",
+    GROUP_INVITE_LINK: "https://whatsapp.com/channel/0029VbDGDej1nozCWekCtb07",
+    MAX_RETRIES: 3,
+    RCD_IMAGE_PATH: "https://files.catbox.moe/bttk8o.jpeg"
+};
